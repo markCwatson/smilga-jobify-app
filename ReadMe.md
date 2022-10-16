f7c3c81bc45210b0d6eda68e78e387234b058570
Mark Watson <markus.c.watson@gmail.com>
2022-10-16 18:13:01 -0300

    setup

---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+<h1>Smilga Jobify App</h1>
+
+<br>
+
+<p>
+This project is based on John Smilga's course "MERN Stack Course 2022 - MongoDB, Express, React and NodeJS" on <a href="https://www.udemy.com/course/mern-stack-course-mongodb-express-react-and-nodejs/">Udemy</a>.
+</p>
+
+<br>
+
+<p>
+More to come as I progress through the course.
+</p>
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
+
+const Landing = () => {
+  return (
+    <main>
+        <nav>
+            <img src={logo} alt='jobify' classname='logo' />
+        </nav>
+
+        <div className='container page'>
+            <div className='info'>
+                <h1>
+                    job <span>tracking</span> app
+                </h1>
+                <p>
+                    Letterpress messenger bag pickled poke. Chartreuse taiyaki poutine salvia kogi. Coloring book hoodie vape intelligentsia, wayfarers tumeric mustache. Trust fund shoreditch Brooklyn, lyft selfies retro plaid banh mi fashion axe asymmetrical small batch mumblecore.
+                </p>
+
+                <button className='btn btn-hero'>
+                    Login/Register
+                </button>
+            </div>
+
+            <img src={main} alt='job hunt' className='img main-img'/>
+        </div>
+    </main>
+  )
+}
+
+export default Landing
