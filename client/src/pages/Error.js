70cd9ede452346fa0a1ac75bd3473ada598f6e78
Mark Watson <markus.c.watson@gmail.com>
2022-10-16 20:35:51 -0300

    error page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,11 +1,16 @@
-import Wrapper from '../assets/wrappers/LandingPage';
-import err from '../assets/images/not-found.svg'
+import { Link } from 'react-router-dom';
+
+import Wrapper from '../assets/wrappers/ErrorPage';
+import err from '../assets/images/not-found.svg';
 
 const Error = () => {
   return (
-    <Wrapper>
-        <div className='container page'>
+    <Wrapper className='full-page'>
+        <div>
             <img src={err} alt='job hunt' className='img main-img'/>
+            <h3>Oops.. page not found!</h3>
+            <p>sorry about that</p>
+            <Link to='/'>Back Home</Link>
         </div>
     </Wrapper>
   )
